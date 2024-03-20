55ffc63a59886ab34cc584318dda20c873b67648
Your Name <you@example.com>
2024-03-20 12:22:44 -0600

    pantallas

---
+++ client/src/components/Tarea.tsx
@@ -0,0 +1,21 @@
+export default function Tarea() {
+    return (
+
+        
+        <tr>
+            <td>1</td>
+            <td>Correr</td>
+            <td>Mañana tengo que correr</td>
+            <td>15-03-2024</td>
+            <td>Pendiente</td>
+            <td>Media</td>
+            <td>
+                <button className="btn btn-primary">Actualizar</button>
+            </td>
+            <td>
+                <button className="btn btn-danger">Eliminar</button>
+            </td>
+        </tr>
+    )
+
+};
